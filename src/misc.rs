66cf0447173fef1026b430e8068848448ccdc30d
huyuncong <huyuncongh@gmail.com>
2022-01-21 16:05:32 -0800

    Update the order of row and col

--- src/misc.rs
+++ src/misc.rs
@@ -268,8 +268,8 @@ pub fn sum_matrices<F: Field>(a: &Matrix<F>, b: &Matrix<F>, c: &Matrix<F>) -> Ve
 #[allow(unused)]
 pub fn joint_matrices<F: Field>(
     joint_matrix: &Vec<Vec<usize>>,
-    num_constraints: usize,
-    num_variables: usize,
+    _num_constraints: usize,
+    _num_variables: usize,
     a: &Matrix<F>,
     b: &Matrix<F>,
     c: &Matrix<F>,
@@ -313,11 +313,11 @@ pub fn joint_matrices<F: Field>(
 
     for (r, row) in joint_matrix.into_iter().enumerate() {
         for i in row {
-            let row_val = F::from((num_constraints - r) as u64);
-            let col_val = F::from((num_variables - *i) as u64);
+            let row_val = F::from(r as u64);
+            let col_val = F::from(*i as u64);
 
-            row_index_vec.push(num_constraints - r);
-            col_index_vec.push(num_variables - *i);
+            row_index_vec.push(r);
+            col_index_vec.push(*i);
             row_vec.push(row_val);
             col_vec.push(col_val);
             // We insert zeros if a matrix doesn't contain an entry at the given (row, col) location.
