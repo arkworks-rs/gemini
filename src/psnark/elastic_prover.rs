717310df0c6a4773b6eb1c2d4f422c30c690ce2d
Michele Orrù <michele.orru@berkeley.edu>
2022-01-27 17:37:32 -0800

    Fix randomness used in elastic protocol.

--- src/psnark/elastic_prover.rs
+++ src/psnark/elastic_prover.rs
@@ -176,12 +176,14 @@ impl<E: PairingEngine> Proof<E> {
         let sumcheck2 = Sumcheck::new_elastic(&mut transcript, z_star, rhs, E::Fr::one());
         // Lookup protocol (plookup) for r_a \subset r, z* \subset r
         let gamma = transcript.get_challenge(b"gamma");
-        let chi = transcript.get_challenge::<E::Fr>(b"chi");
-        let zeta = transcript.get_challenge(b"zeta");
+        let chi = transcript.get_challenge(b"chi");
+        let zeta = transcript.get_challenge::<E::Fr>(b"zeta");
+
         let (pl_set_alpha, pl_subset_alpha, pl_sorted_alpha) =
-            plookup_streams(&alphas, &alpha_star, &row, gamma, zeta);
-        let (pl_set_r, pl_subset_r, pl_sorted_r) = plookup_streams(&rs, &r_star, &col, gamma, zeta);
-        let (pl_set_z, pl_subset_z, pl_sorted_z) = plookup_streams(&r1cs.z, &z_star, &row, gamma, zeta);
+            plookup_streams(&alpha_star, &alphas, &row, gamma, chi);
+        let (pl_set_r, pl_subset_r, pl_sorted_r) = plookup_streams(&r_star, &rs, &row, gamma, chi);
+        let (pl_set_z, pl_subset_z, pl_sorted_z) =
+            plookup_streams( &z_star, &r1cs.z, &row, gamma, chi);
         // compute the products to send to the verifier.
         // XXXX. There is no need to compute the sorted ones as they can be derived.
         let set_alpha_ep = pl_set_alpha.iter().product();
