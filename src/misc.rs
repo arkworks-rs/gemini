78799a5c5828e53cdeb2fcca39afef647c8ef413
Annamira O'Toole <annamira@uname.net>
2022-10-25 00:11:33 -0700

    Update arkworks-rs/snark source, add misc.rs multikzg updates

--- src/misc.rs
+++ src/misc.rs
@@ -1,10 +1,13 @@
-use ark_ff::Field;
+use ark_ff::{Field, PrimeField};
 use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial};
+use ark_poly::multivariate::SparsePolynomial;
+use ark_poly::multivariate::Term;
 use ark_std::borrow::Borrow;
 use ark_std::vec::Vec;
 
 use crate::circuit::Matrix;
-use ark_std::collections::{BTreeMap, BTreeSet};
+use ark_std::collections::{BTreeMap, BTreeSet, HashSet};
+use ark_std::rand::RngCore;
 
 pub(crate) const TENSOR_EXPANSION_LOG: usize = 16;
 pub(crate) const TENSOR_EXPANSION: usize = (1 << TENSOR_EXPANSION_LOG) - 1;
@@ -45,6 +48,190 @@ where
         .reduce(|x, y| x + y)
         .map(|x| x.coeffs.into())
         .unwrap_or_else(|| Vec::new())
+}
+
+/// Given as input `elements`, an array of field elements
+/// \\(\rho_0, \dots, \rho_{n-1}\\)
+/// compute the tensor product
+/// \\( \otimes_j (1, \rho_j )\\)
+pub fn tensor<F: Field>(elements: &[F]) -> Vec<F> {
+    assert!(!elements.is_empty());
+    let mut tensor = vec![F::one(); 1 << elements.len()];
+    let mut elements_iterator = elements.iter().enumerate();
+
+    tensor[1] = *elements_iterator
+        .next()
+        .expect("Expecting at least one element in the tensor product.")
+        .1;
+    // guaranteed to have at least one element.
+
+    for (i, element) in elements_iterator {
+        for j in 0..1 << i {
+            tensor[(1 << i) + j] = tensor[j] * element;
+        }
+    }
+    tensor
+}
+
+pub(crate) type PartialTensor<F> = Vec<Vec<F>>;
+
+/// Partially expand the tensor product
+/// \\(\otimes (1, \rho_j)\\)
+/// XXX TODO: This function is pub(crate) as in a previous version of this library,
+/// Iterable: Copy and hence couldn't store vectors itself.
+/// This is not anymore the case thus it can be moved inside init.
+pub fn expand_tensor<F: Field>(elements: &[F]) -> PartialTensor<F> {
+    // expected_len = ceil(tensor_len / N)
+    let expected_len = ceil_div(elements.len(), TENSOR_EXPANSION_LOG);
+    let mut expanded_tensor = Vec::with_capacity(expected_len);
+
+    for i in 0..expected_len {
+        let mut got = if (i + 1) * TENSOR_EXPANSION_LOG <= elements.len() {
+            tensor(&elements[i * TENSOR_EXPANSION_LOG..(i + 1) * TENSOR_EXPANSION_LOG])
+        } else {
+            tensor(&elements[i * TENSOR_EXPANSION_LOG..])
+        };
+        // remove the first element (1) that is the tensor with no element.
+        got.remove(0);
+        expanded_tensor.push(got);
+    }
+
+    expanded_tensor
+}
+
+/// For a slice x, multiply the components of x that correlate to the bits of idx.
+/// From Michele's expression of multivariate polynomial in single dimension vector
+///
+pub fn mul_components<F>(x: &[F], idx: usize) -> F
+where
+    F: Field,
+{
+    let mut result = F::one();
+    for (i, x_i) in x.iter().enumerate() {
+        if (idx >> i) & 1 == 1 {
+            result = result.mul(x_i);
+        }
+    }
+    result
+}
+
+/// Generate a random vector of field elements such that they're all different
+pub fn random_unique_vector<F>(size: usize, rng: &mut impl RngCore) -> Vec<F>
+where
+    F: Field,
+{
+    let mut result = HashSet::new();
+    while result.len() < size {
+        result.insert(F::rand(rng));
+    }
+    result.into_iter().collect::<Vec<_>>()
+}
+
+pub fn random_vector<F>(size: usize, rng: &mut impl RngCore) -> Vec<F>
+where
+    F: Field,
+{
+    let mut result = Vec::with_capacity(size);
+    for _ in 0..size {
+        result.push(F::rand(rng));
+    }
+    result
+}
+
+/// Polynomial evaluation for a multidimensional polynomial, encoded as Michele described.
+/// TODO: faster implementation using multi_poly_vector_expand
+///
+pub fn evaluate_multi_poly<F>(polynomial: &[F], x: &[F]) -> F
+where
+    F: Field,
+{
+    // check that lengths line up
+    assert_eq!(polynomial.len(), 1 << x.len());
+    let polynomial_iterator = polynomial.iter().enumerate();
+    let mut result = F::zero();
+    let tensor_x = tensor(x);
+    for (i, polynomial_i) in polynomial_iterator {
+        result += tensor_x[i] * polynomial_i;
+    }
+    result
+}
+
+#[test]
+fn test_evaluate_multi_poly() {
+    use crate::ark_std::One;
+    use ark_bls12_381::Fr;
+    let coefs_ones = [Fr::one(); 16];
+    let point_ones = [Fr::one(); 4];
+    let mut result = evaluate_multi_poly(&coefs_ones, &point_ones);
+    assert_eq!(result, Fr::from(16));
+    println!("result: {}", evaluate_multi_poly(&coefs_ones, &point_ones));
+
+    // This evaluation should be 112. I chose these
+    let coefs = [0_u64, 6, 9, 2, 4, 8, 5, 1];
+    let point = [4_u64, 2, 1];
+    // How can I turn this into a function
+    let coefs_fr = coefs.into_iter().map(|x| Fr::from(x)).collect::<Vec<_>>();
+    let point_fr = point.into_iter().map(|x| Fr::from(x)).collect::<Vec<_>>();
+
+    let expected_evaluation = coefs_fr[0]
+        + coefs_fr[1] * point_fr[0]
+        + coefs_fr[2] * point_fr[1]
+        + coefs_fr[3] * point_fr[0] * point_fr[1]
+        + coefs_fr[4] * point_fr[2]
+        + coefs_fr[5] * point_fr[2] * point_fr[0]
+        + coefs_fr[6] * point_fr[2] * point_fr[1]
+        + coefs_fr[7] * point_fr[2] * point_fr[1] * point_fr[0];
+
+    result = evaluate_multi_poly(&coefs_fr, &point_fr);
+    assert_eq!(result, expected_evaluation);
+}
+
+/// divide polynomials
+/// for evaluation point \\(alpha_1, alpha_2, ... alpha_n\\)
+/// get some polynomials \\(q_1, ... q_n\\) such that polynomial = \\(\sum_i{q_i(x)(x_i - \alpha_i)} + f(\alpha)\\)
+/// polynomial is ordered f0, f1, f2, .... f_N
+/// Take the binary expansion of i: b0, b1, ... bn. If b_j is 1, then multiply f_i by x_j.
+///
+pub fn multi_poly_decompose<F: PrimeField>(polynomial: &[F], eval_point: &[F]) -> (Vec<Vec<F>>, F) {
+    let mut results = Vec::new();
+    for _i in 0..eval_point.len() {
+        results.push(vec![F::zero(); polynomial.len()]);
+    }
+    let mut evaluation = F::zero();
+    for (ind, item) in polynomial.iter().enumerate() {
+        // if the i'th bit of j is 1, that means it shouldn't be in the quotient
+        let mut term = *item;
+        let mut ind_shifted = ind;
+        let _counter = 0;
+        for dim in 0..eval_point.len() {
+            if ind_shifted == 0 {
+                break;
+            }
+            if ind_shifted & 1 == 1 {
+                let dim_bit_off = (ind_shifted >> 1) << (1 + dim);
+                results[dim][dim_bit_off] += term;
+                term *= eval_point[dim];
+            }
+            ind_shifted >>= 1;
+        }
+        evaluation += term;
+    }
+
+    // assert_eq!(evaluate_multi_poly(&remainder, evaluation_point), remainder[0]);
+    (results, evaluation)
+}
+
+/// Converts a sparse_polynomial from ark_poly into a vector of coefficients
+pub fn multi_poly_flatten<F: Field, T: Term>(sparse_polynomial: &SparsePolynomial<F, T>) -> Vec<F> {
+    let mut f = vec![F::zero(); 1 << sparse_polynomial.num_vars];
+    for term in &sparse_polynomial.terms {
+        let mut idx: usize = 0;
+        for term_idx in term.1.vars() {
+            idx += 1 << term_idx;
+        }
+        f[idx] = term.0;
+    }
+    f
 }
 
 /// Helper function for folding single polynomial.
@@ -126,53 +313,7 @@ pub fn product_vector_matrix<F: Field>(z: &[F], matrix: &[Vec<(F, usize)>]) -> V
     res
 }
 
-/// Given as input `elements`, an array of field elements
-/// \\(\rho_0, \dots, \rho_{n-1}\\)
-/// compute the tensor product
-/// \\( \otimes_j (1, \rho_j )\\)
-pub fn tensor<F: Field>(elements: &[F]) -> Vec<F> {
-    assert!(!elements.is_empty());
-    let mut tensor = vec![F::one(); 1 << elements.len()];
-    let mut elements_iterator = elements.iter().enumerate();
 
-    tensor[1] = *elements_iterator
-        .next()
-        .expect("Expecting at lest one element in the tensor product.")
-        .1;
-    // guaranteed to have at least one element.
-    for (i, element) in elements_iterator {
-        for j in 0..1 << i {
-            tensor[(1 << i) + j] = tensor[j] * element;
-        }
-    }
-    tensor
-}
-
-pub(crate) type PartialTensor<F> = Vec<Vec<F>>;
-
-/// Partially expand the tensor product
-/// \\(\otimes (1, \rho_j)\\)
-/// XXX TODO: This function is pub(crate) as in a previous version of this library,
-/// Iterable: Copy and hence couldn't store vectors itself.
-/// This is not anymore the case thus it can be moved inside init.
-pub fn expand_tensor<F: Field>(elements: &[F]) -> PartialTensor<F> {
-    // expected_len = ceil(tensor_len / N)
-    let expected_len = ceil_div(elements.len(), TENSOR_EXPANSION_LOG);
-    let mut expanded_tensor = Vec::with_capacity(expected_len);
-
-    for i in 0..expected_len {
-        let mut got = if (i + 1) * TENSOR_EXPANSION_LOG <= elements.len() {
-            tensor(&elements[i * TENSOR_EXPANSION_LOG..(i + 1) * TENSOR_EXPANSION_LOG])
-        } else {
-            tensor(&elements[i * TENSOR_EXPANSION_LOG..])
-        };
-        // remove the first element (1) that is the tensor with no element.
-        got.remove(0);
-        expanded_tensor.push(got);
-    }
-
-    expanded_tensor
-}
 
 /// Polynomial evaluation, assuming that the
 /// coeffients are in big-endian.
@@ -411,4 +552,39 @@ fn test_evaluate_index_poly() {
     let got = evaluate_index_poly(x, n);
     let expected = evaluate_le(&index_polynomial, &x);
     assert_eq!(got, expected);
+}
+
+#[test]
+fn test_multi_poly_decompose() {
+    use crate::ark_std::Zero;
+    use ark_bls12_381::Fr;
+    let polynomial_small = vec![Fr::from(2_i32), Fr::from(3_i32)];
+    let eval_point_small: Vec<Fr> = vec![Fr::from(6_i32)];
+    let (quotients_small, remainder_small) =
+        multi_poly_decompose(&polynomial_small, &eval_point_small);
+    println!("testing small values");
+    assert_eq!(quotients_small[0][0], Fr::from(3_i32));
+    assert_eq!(remainder_small, Fr::from(20_i32));
+    println!("testing random values");
+    let reps = 3;
+    let dim = 12;
+    let rng = &mut ark_std::test_rng();
+    let polynomial: Vec<Fr> = random_vector(1 << dim, rng);
+    let eval_point: Vec<Fr> = random_vector(dim, rng);
+
+    let (quotients, remainder) = multi_poly_decompose(&polynomial, &eval_point);
+    for _ in 0..reps {
+        let test_point: Vec<Fr> = random_unique_vector(dim, rng);
+        let mut sum = Fr::zero();
+        for (idx, quotient) in quotients.iter().enumerate() {
+            let eval_quotient = evaluate_multi_poly(quotient, &test_point);
+
+            // sum (q_i(x) * (x_i - alpha_i)) + remainder = f(x)
+            sum += eval_quotient * (test_point[idx] - eval_point[idx]);
+        }
+        assert_eq!(
+            sum + remainder,
+            evaluate_multi_poly(&polynomial, &test_point)
+        );
+    }
 }
